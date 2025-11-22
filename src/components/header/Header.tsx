25cd67704f74b3f0134e1f953e3c93ef377f381f
PetJs <peterfagoroye76@gmail.com>
2025-11-22 11:30:12 +0100

    feat: Adds Profile and Notification routes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,26 @@
 import { Button } from "../ui/button";
 import { Bell } from "lucide-react";
 import { User } from "lucide-react";
+import { SidebarTrigger } from "../ui/sidebar";
+import { Link } from "react-router";
 
 const Header = () =>  {
     return (
-        <div className="max-h-[80px] fixed top-0 left-0 flex items-center justify-end py-2 px-1 gap-2 w-full bg-white">
-            <Button className="bg-primary-800 ">Connect Wallet</Button>
-            <div className="flex gap-3 items-center ">
-                <div className="w-10 h-10 flex items-center justify-center rounded-full border border-2 border-primary-200">
-                    <Bell size={20}/>
-                </div>
-                <div className="w-10 h-10 flex items-center justify-center rounded-full border border-2 border-primary-200">
-                    <User size={20}/>
+        <div className="max-h-[80px] w-full bg-white fixed top-0 left-0 flex justify-between items-center">
+            <SidebarTrigger/>
+            <div className="  flex items-center justify-end py-2 px-1 gap-2 ">
+                <Button className="bg-primary-800 ">Connect Wallet</Button>
+                <div className="flex gap-3 items-center ">
+                    <div className="w-10 h-10 flex items-center justify-center rounded-full border border-2 border-primary-200">
+                        <Link to="/notifications">
+                            <Bell size={20}/>
+                        </Link>
+                    </div>
+                    <div className="w-10 h-10 flex items-center justify-center rounded-full border border-2 border-primary-200">
+                        <Link to="/profile">
+                            <User size={20}/>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
