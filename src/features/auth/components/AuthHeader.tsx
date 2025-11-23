f7971ea4938cb7f53f568fcb927fb089f15beed7
Susu-spec <suleimansuwaibat@gmail.com>
2025-11-23 09:51:05 +0100

    refactor: separates components on auth pages

---
+++ src/features/auth/components/AuthHeader.tsx
@@ -0,0 +1,24 @@
+import type { AuthHeaderProps } from "../types";
+import { Link } from "react-router";
+
+export function AuthHeader({
+    title,
+    subtitle,
+    linkText,
+    linkTo
+}: AuthHeaderProps) {
+  return (
+    <div className="flex flex-col items-center gap-1.75 md:gap-3">
+        <h1 className="font-sora text-2xl md:text-3xl lg:text-5xl font-bold md:font-semibold leading-9 lg:leading-18 text-center">
+            {title}
+        </h1>
+
+        <p className="text-sm md:text-lg flex flex-wrap gap-1 justify-center">
+            {subtitle}
+            <Link to={linkTo} className="text-primary-900 font-semibold">
+                {linkText}
+            </Link>
+        </p>
+    </div>
+  );
+}
