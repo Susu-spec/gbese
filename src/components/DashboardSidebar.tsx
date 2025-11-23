cb21b053afd27cca4826ce637730aadfcddd3ee1
PetJs <peterfagoroye76@gmail.com>
2025-11-23 15:53:56 +0100

    style: Refactor DashboardSidebar for improved structure and readability

--- src/components/DashboardSidebar.tsx
+++ src/components/DashboardSidebar.tsx
@@ -19,17 +19,17 @@ export function DashboardSidebar() {
     return (
         <Sidebar className="z-40">
             <SidebarHeader className="mb-4 text-white">Gbese</SidebarHeader>
-            <SidebarContent>
-                <SidebarGroup >
+            <SidebarContent className="max-w-62 hide-scrollbar overflow-y-auto ">
+                <SidebarGroup>
                     <SidebarGroupContent>
-                        <SidebarMenu  className="gap-3" >
+                        <SidebarMenu  className="gap-2" >
                             {protectedRoutes
                                 .filter(item => item.path !== "profile" && item.path !== "notifications")
                                 .map((item) => {
                                     const active = location.pathname === `/${item.path}`;
                                     return(
-                                        <SidebarMenuItem key={item.path} className="text-white hover:bg-white hover:rounded-md">
-                                                <SidebarMenuButton asChild isActive={active} className="p-2 ">
+                                        <SidebarMenuItem key={item.path} className="text-white hover:bg-white hover:rounded-md  ">
+                                                <SidebarMenuButton asChild isActive={active} className="px-6 py-4 h-14 ">
                                                     <Link to={`/${item.path}`}>
                                                         <item.icon />
                                                         <span >{item.label}</span>
