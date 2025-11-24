5d4bbf82f59358f6841e49171013e95eefe58266
PetJs <peterfagoroye76@gmail.com>
2025-11-24 02:19:05 +0100

    style: Refactor DashboardSidebar to use NavLink for improved routing

--- src/components/DashboardSidebar.tsx
+++ src/components/DashboardSidebar.tsx
@@ -1,5 +1,5 @@
-import { protectedRoutes } from "@/routes/protected"
-import { Link, useLocation } from "react-router"
+import { protectedRoutes } from "@/routes/protected";
+import {NavLink} from "react-router";
 
 import {
   Sidebar,
@@ -10,39 +10,47 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-} from "@/components/ui/sidebar"
-
+} from "@/components/ui/sidebar";
 
 export function DashboardSidebar() {
-    const location = useLocation();
 
-    return (
-        <Sidebar className="z-40">
-            <SidebarHeader className="mb-4 text-white">Gbese</SidebarHeader>
-            <SidebarContent className="max-w-62 hide-scrollbar overflow-y-auto ">
-                <SidebarGroup>
-                    <SidebarGroupContent>
-                        <SidebarMenu  className="gap-2" >
-                            {protectedRoutes
-                                .filter(item => item.path !== "profile" && item.path !== "notifications")
-                                .map((item) => {
-                                    const active = location.pathname === `/${item.path}`;
-                                    return(
-                                        <SidebarMenuItem key={item.path} className="text-white hover:bg-white hover:rounded-md  ">
-                                                <SidebarMenuButton asChild isActive={active} className="px-6 py-4 h-14 ">
-                                                    <Link to={`/${item.path}`}>
-                                                        <item.icon />
-                                                        <span >{item.label}</span>
-                                                    </Link>
-                                                </SidebarMenuButton>
-                                        </SidebarMenuItem>
-                                    )
-                                }
-                            )}
-                        </SidebarMenu>
-                    </SidebarGroupContent>
-                </SidebarGroup>
-            </SidebarContent>
-        </Sidebar>
-    )
+  return (
+    <Sidebar className="z-40">
+      <SidebarHeader className="mb-4 text-white">Gbese</SidebarHeader>
+      <SidebarContent className="max-w-62 hide-scrollbar overflow-y-auto ">
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu className="gap-2">
+              {protectedRoutes
+                .filter(
+                  (item) =>
+                    item.path !== "profile" && item.path !== "notifications"
+                )
+                .map((item) => (
+                  <SidebarMenuItem
+                    key={item.path}
+                    className="text-white hover:bg-white hover:rounded-md"
+                  >
+                    <NavLink to={`/${item.path}`} end>
+                      {({ isActive }) => (
+                        <SidebarMenuButton
+                          asChild
+                          isActive={isActive}
+                          className="px-6 py-4 h-14"
+                        >
+                          <div>
+                            <item.icon />
+                            <span>{item.label}</span>
+                          </div>
+                        </SidebarMenuButton>
+                      )}
+                    </NavLink>
+                  </SidebarMenuItem>
+                ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
 }
