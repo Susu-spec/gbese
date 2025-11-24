751db32580f34b27bf6641c7c35d03c64d32600d
PetJs <peterfagoroye76@gmail.com>
2025-11-24 11:42:49 +0100

    style: Add Gbese logo to DashboardSidebar header for enhanced branding

--- src/components/DashboardSidebar.tsx
+++ src/components/DashboardSidebar.tsx
@@ -1,5 +1,6 @@
 import { protectedRoutes } from "@/routes/protected";
 import {NavLink} from "react-router";
+import GbeseIcon from "/icons/gbese-logo.svg"
 
 import {
   Sidebar,
@@ -16,41 +17,46 @@ export function DashboardSidebar() {
 
   return (
     <Sidebar className="z-40">
-      <SidebarHeader className="mb-4 text-white">Gbese</SidebarHeader>
-      <SidebarContent className="max-w-62 hide-scrollbar overflow-y-auto ">
-        <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarMenu className="gap-2">
-              {protectedRoutes
-                .filter(
-                  (item) =>
-                    item.path !== "profile" && item.path !== "notifications"
-                )
-                .map((item) => (
-                  <SidebarMenuItem
-                    key={item.path}
-                    className="text-white hover:bg-white hover:rounded-md"
-                  >
-                    <NavLink to={`/${item.path}`} end>
-                      {({ isActive }) => (
-                        <SidebarMenuButton
-                          asChild
-                          isActive={isActive}
-                          className="px-6 py-4 h-14"
+        <SidebarHeader className="mb-4 text-white ">
+            <div className=" flex items-center gap-4 p-2">
+                <img src={GbeseIcon} alt="Gbese Logo" />
+                <h2 className="text-lg">Gbese</h2>
+            </div>
+        </SidebarHeader>
+        <SidebarContent className="max-w-62 hide-scrollbar overflow-y-auto ">
+            <SidebarGroup>
+                <SidebarGroupContent>
+                    <SidebarMenu className="gap-2">
+                    {protectedRoutes
+                        .filter(
+                        (item) =>
+                            item.path !== "profile" && item.path !== "notifications"
+                        )
+                        .map((item) => (
+                        <SidebarMenuItem
+                            key={item.path}
+                            className="text-white hover:bg-white hover:rounded-md"
                         >
-                          <div>
-                            <item.icon />
-                            <span>{item.label}</span>
-                          </div>
-                        </SidebarMenuButton>
-                      )}
-                    </NavLink>
-                  </SidebarMenuItem>
-                ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
+                            <NavLink to={`/${item.path}`} end>
+                            {({ isActive }) => (
+                                <SidebarMenuButton
+                                asChild
+                                isActive={isActive}
+                                className="px-6 py-4 h-14"
+                                >
+                                <div>
+                                    <item.icon />
+                                    <span>{item.label}</span>
+                                </div>
+                                </SidebarMenuButton>
+                            )}
+                            </NavLink>
+                        </SidebarMenuItem>
+                        ))}
+                    </SidebarMenu>
+                </SidebarGroupContent>
+            </SidebarGroup>
+        </SidebarContent>
     </Sidebar>
   );
 }
