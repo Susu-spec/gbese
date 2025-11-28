700ab7358043ba44e5fee45c9a1cb6ea9355bb7c
PetJs <peterfagoroye76@gmail.com>
2025-11-28 17:46:16 +0100

    feat: enhance TransferredDebts component with improved data handling and formatting

--- src/features/main/my-debts/components/TransferredDebts.tsx
+++ src/features/main/my-debts/components/TransferredDebts.tsx
@@ -1,13 +1,15 @@
 import { Card } from "@/components/ui/card";
 import type { DebtRequest } from "../../dashboard/types";
 import { useDebt } from "../hook"
+import { formatDate } from "@/lib/utils";
 
 
 const TransferredDebts = () => {
     const {transferredDebtsQuery} = useDebt();
     const isLoading = transferredDebtsQuery.isPending;
 
-    const transferredDebts = transferredDebtsQuery.data || [];
+    const transferredDebts = transferredDebtsQuery.data?.data || [];
+    console.log("Transferred Debts:", transferredDebts);
     return (
         <div>
             <h2 className="text-3xl text-primary-800">Transferred Debts</h2>
@@ -15,9 +17,9 @@ const TransferredDebts = () => {
             {!isLoading && transferredDebts.length === 0 && <p className="text-gray-500">No transferred debts found.</p>}
             {!isLoading && transferredDebts.length > 0 && (
                 transferredDebts.map((debt: DebtRequest) => (
-                    <Card key={debt.id} className="mb-4 ">
+                    <Card key={debt.id} className="mb-4 p-4">
                         <div className="flex justify-between items-center">
-                            <h2>{debt.id}</h2>
+                            <h2>{((debt.id) as string).substring(0, 10)}...</h2>
                             <p 
                             className="rounded-xl p-2 w-fit"
                             style={{
@@ -37,10 +39,10 @@ const TransferredDebts = () => {
                             >{debt.status}</p>
                         </div>
                         <p className="text-2xl font-bold">{debt.debt.principal_amount}</p>
-                        <p className="text-xs text-gray-400">{debt.recipient.first_name} {debt.recipient.last_name}</p>
+                        <p className="text-sm text-gray-400">{debt.recipient.first_name} {debt.recipient.last_name}</p>
                         <div>
                             <p className="text-sm text-gray-500">
-                                Transferred on: {debt.debt.transferred_at ? new Date(debt.debt.transferred_at).toLocaleDateString() : 'N/A'}
+                                Transferred on: {debt.debt.transferred_at ? formatDate(debt.debt.transferred_at) : 'N/A'}
                             </p>
                         </div>
                     </Card>
