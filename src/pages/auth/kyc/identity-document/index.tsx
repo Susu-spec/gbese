38f7e147ad1d91a2bb5b8d7117f92cb13aa0a898
Susu-spec <suleimansuwaibat@gmail.com>
2025-11-30 16:00:04 +0100

    feat: adds forms and pages for kyc flow

--- src/pages/auth/kyc/identity-document/index.tsx
+++ src/pages/auth/kyc/identity-document/index.tsx
@@ -1,5 +1,24 @@
+import IdentityDocumentForm from "@/features/kyc/components/IdentityDocumentForm";
+
 export default function IdentityDocument() {
     return (
-        <></>
+        <div className="flex flex-col gap-4.5 md:gap-10">
+            <div className="flex flex-col gap-4 md:gap-8">
+                <div className="flex flex-col gap-2">
+                    <h1 className="font-semibold text-xl md:text-[2rem]">
+                        Identity Document
+                    </h1>
+                    <p className="text-xs md:text-sm">
+                        Please upload a valid ID document (Passport, Driver's license, or National ID card)
+                    </p>
+                </div>
+                <div className="py-5 px-6 bg-primary-150 rounded-xl">
+                    <p className="text-xs md:text-sm">
+                        Please upload a clear photo or scan of your ID, ensuring your name, photo, and ID number are fully visible without glare, blur, or cropping
+                    </p>
+                </div>
+            </div>
+            <IdentityDocumentForm />
+        </div>
     )
 }
