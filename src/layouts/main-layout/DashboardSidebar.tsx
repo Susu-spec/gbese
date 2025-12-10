adb3bdcd78797185d346f9c717ddf14666cd8ddf
Susu-spec <suleimansuwaibat@gmail.com>
2025-12-10 16:31:11 +0100

    fix: fixes dashboard sidebar layout

--- src/layouts/main-layout/DashboardSidebar.tsx
+++ src/layouts/main-layout/DashboardSidebar.tsx
@@ -22,50 +22,51 @@ export function DashboardSidebar() {
     const { setOpenMobile } = useSidebar();
 
   return (
-    <Sidebar className="z-40">
-        <SidebarHeader className="mb-4 text-white ">
-            <div className=" flex items-center gap-4 p-2">
-                <img src={GbeseIcon} alt="Gbese Logo" />
+    <Sidebar className="z-40 max-w-73.75 flex flex-col gap-9">
+        <SidebarHeader className="text-white">
+            <div className="flex items-center gap-1 p-2">
+                <img src={GbeseIcon} alt="Gbese Logo" className="w-8.5 h-auto"/>
                 <h2 className="text-lg">Gbese</h2>
             </div>
         </SidebarHeader>
-        <SidebarContent className="max-w-62 hide-scrollbar overflow-y-auto ">
+
+        <SidebarContent className="hide-scrollbar overflow-y-auto flex-1">
             <SidebarGroup>
                 <SidebarGroupContent>
                     <SidebarMenu className="gap-2">
-                    {protectedRoutes
-                        .filter(item => item.showInSidebar)
-                        .map((item) => (
-                        <SidebarMenuItem
-                            key={item.path}
-                            className="text-white hover:bg-white hover:rounded-md"
-                        >
-                            <NavLink to={`/${item.path}`}>
-                            {({ isActive }) => (
-                                <SidebarMenuButton
-                                asChild
-                                isActive={isActive}
-                                className="px-6 py-4 h-14"
-                                onClick={() => setOpenMobile(false)}
+                        {protectedRoutes
+                            .filter(r => r.showInSidebar)
+                            .map(item => (
+                                <SidebarMenuItem 
+                                    key={item.path}
+                                    className="hover:bg-white hover:rounded-md max-w-11/12"
                                 >
-                                <div>
-                                    {item.icon && <item.icon />}
-                                    <span>{item.label}</span>
-                                </div>
-                                </SidebarMenuButton>
-                            )}
-                            </NavLink>
-                        </SidebarMenuItem>
-                        ))}
+                                    <NavLink to={`/${item.path}`}>
+                                        {({ isActive }) => (
+                                            <SidebarMenuButton
+                                                asChild
+                                                isActive={isActive}
+                                                className="text-gbese-neutrals-100 px-6 py-2.5 flex gap-3 leading-6.75 h-fit"
+                                                onClick={() => setOpenMobile(false)}
+                                            >
+                                                <span className="flex items-center gap-2">
+                                                    {item.icon && <item.icon />}
+                                                    <span>{item.label}</span>
+                                                </span>
+                                            </SidebarMenuButton>
+                                        )}
+                                    </NavLink>
+                                </SidebarMenuItem>
+                            ))}
                     </SidebarMenu>
                 </SidebarGroupContent>
             </SidebarGroup>
         </SidebarContent>
-    
-        <SidebarFooter>
+        
+        <SidebarFooter className="mt-auto pt-4">
             <button 
                 title="Log out" 
-                className="flex flex-row items-center gap-2 text-gbese-white px-6 pb-4 text-sm"
+                className="flex items-center gap-2 text-gbese-white px-3.5 pb-4 text-sm"
                 onClick={signOut}
             >
                 <LogOutIcon className="text-gbese-white" size={20}/>
